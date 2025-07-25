e8fff8b3f73f8f2ce297cc6507932a328b956bcc
etie20 <etienne.foyang@2026.ucac-icam.com>
2025-07-25 02:34:39 +0100

    dashboard ui

--- track1Backend/reminder/src/main/java/com/code2care/reminder/infrastructure/controller/ReminderController.java
+++ track1Backend/reminder/src/main/java/com/code2care/reminder/infrastructure/controller/ReminderController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("reminder")
+@RequestMapping("api/reminder")
 @CrossOrigin("*")
 class ReminderController {
     private final GetRemindersByDoctorIDUseCase getRemindersByDoctorIDUseCase;
