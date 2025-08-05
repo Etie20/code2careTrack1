e5e3e61c9ba88e9cfcc6bf79ed9f2eb0310949fc
etie20 <etienne.foyang@2026.ucac-icam.com>
2025-08-06 00:47:08 +0100

    hotfixes track1 #2

--- dashboard/src/app/environment/environment.ts
+++ dashboard/src/app/environment/environment.ts
@@ -3,4 +3,4 @@ export const environment = {
   title: 'HealthCare Companion',
   apiUrl: 'https://code2caretrack1.onrender.com/api',
   analyticsApiUrl: 'https://code2care-fastapi-latest.onrender.com/api/v1'
-};
+}
